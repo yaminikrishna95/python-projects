4e2abd161006d0f0b71e370e6d734da9e760efa3
yamini <yaminikrishna.1995@gmail.com>
2026-03-11 16:46:37 -0500

    oops practice

--- OOPS_Practice/main.py
+++ OOPS_Practice/main.py
@@ -3,33 +3,48 @@ from six import raise_from
 
 class Employee:
 	num_of_emps = 0
-	def __init__(self,first,last,pay):
-		self.first = first
-	raise_amount = 1.04
+
+
 	def __init__(self,first,last,pay):
 		self.first = first
 		self.last = last
 		self.pay=pay
 		self.email = first +"."+last+"@company.com"
 		Employee.num_of_emps+=1
+
 	def fullname(self):
 		return self.first+" "+self.last
+
 	def apply_raise(self):
 		self.pay = int(self.pay * self.raise_amount )
 		return(self.pay)
+
 	@classmethod
 	def set_raise_amount(cls,amount):
 		cls.raise_amount = amount
+
+	@classmethod
+	def from_string(cls,emp_str):
+		first,last,pay = emp_str.split("-")
+		return cls(first,last,pay)
+	@staticmethod
+	def is_workday(day):
+		if day.weekday()==5 or day.weekday()==6:
+			return False
+		return True
 
 
 
 emp_1 = Employee("James","Smith",50000)
 emp_2 = Employee("laura","Smith",60000)
-Employee.set_raise_amount(1.05)
-emp_2.raise_amount = 1.06
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
 
+emp_str_1="John-Doe-70000"
+emp_str_1= Employee.from_string(emp_str_1)
+print(emp_str_1.fullname())
+
+import datetime
+my_datetime = datetime.datetime(2026,9,11)
+print(Employee.is_workday(my_datetime))
 
 
 
