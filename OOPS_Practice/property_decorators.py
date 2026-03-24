60a30bc58549beb9e4a833fea4e6eed4aa5f6cf8
yamini <yaminikrishna.1995@gmail.com>
2026-03-24 14:52:59 -0500

    oops practice

--- OOPS_Practice/property_decorators.py
+++ OOPS_Practice/property_decorators.py
@@ -1,19 +1,31 @@
+from zmq.backend import first
+
+
 class Employee:
 
 	def __init__(self,first,last,pay):
 		self.first = first
 		self.last = last
 		self.pay = pay
-
+	@property
 	def email(self):
-		return self.first + "." + self.last + "@gmail.com"
+		return '{}.{}@gmail.com'.format(self.first,self.last)
+	@property
 	def fullname(self):
 		return self.first + " " + self.last
 
+	@fullname.setter
+	def fullname(self,name):
+		first,last = name.split(" ")
+		self.first = first
+		self.last = last
+
 
 emp_1=Employee("James","Smith",50000)
+
 emp_1.first="Jim"
+
 print(emp_1.first)
-print(emp_1.email())
-print(emp_1.fullname())
+print(emp_1.email)
+print(emp_1.fullname)
 
