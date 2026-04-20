165a6147e1a7513c4432a8dba780d9d360d0c22b
yamini <yaminikrishna.1995@gmail.com>
2026-04-20 16:17:29 -0500

    asynio

---
+++ Asyncio/example_5.py
@@ -0,0 +1,28 @@
+import asyncio
+import time
+
+
+async def fetch_data(param):
+    print(f"Do something with {param}...")
+    time.sleep(param)
+    print(f"Done with {param}")
+    return f"Result of {param}"
+
+
+async def main():
+    task1 = asyncio.create_task(fetch_data(1))
+    task2 = asyncio.create_task(fetch_data(2))
+    result1 = await task1
+    print("Task 1 fully completed")
+    result2 = await task2
+    print("Task 2 fully completed")
+    return [result1, result2]
+
+
+t1 = time.perf_counter()
+
+results = asyncio.run(main())
+print(results)
+
+t2 = time.perf_counter()
+print(f"Finished in {t2 - t1:.2f} seconds")
